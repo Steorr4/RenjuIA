aab4e8620030d649c2eb39780fbc97cf2e9573ad
“Steorra” <snows.gt0@gmail.com>
2025-03-20 00:17:57 +0100

    Expérimentations pour l'interface graphique.

--- game/Board.py
+++ game/Board.py
@@ -2,22 +2,47 @@ import pygame
 
 S_HSIZE, S_VSIZE = 1200, 900
 COLOR_BG = (237,188,102)
+COLOR_TEST = (255,255,255,0)
 
 class Board :
     def __init__(self):
         # Matrice du plateau
-        self.matrix = [[0] * 15 for i in range(15)]
+        self.matrix = [[0] * 15 for _ in range(15)]
 
         # Relatif a l'affichage
-        # pygame.display.set_caption("Renju")
-        # self.screen = pygame.display.set_mode((S_HSIZE, S_VSIZE))
-        # self.screen.fill(COLOR_BG)
-        # #pygame.draw.rect(self.screen, COLOR_BG, pygame.Rect(S_HSIZE-300,0, S_HSIZE - S_VSIZE, S_VSIZE))
-        # self.board = pygame.transform.scale(pygame.image.load("./game/assets/board.png"), (S_HSIZE-300, S_HSIZE-300))
-        #
-        # pygame.display.flip()
-
-
-    def draw(self):
+        pygame.display.set_caption("Renju")
+        pygame.display.set_icon(pygame.image.load("./game/assets/icon.png"))
+        self.screen = pygame.display.set_mode((S_HSIZE, S_VSIZE))
+        self.screen.fill(COLOR_BG)
+        self.board = pygame.transform.scale(pygame.image.load("./game/assets/board.png"), (S_HSIZE-300, S_HSIZE-300))
         self.screen.blit(self.board, (0, 0))
-        pygame.display.update()
+
+        '''
+        En gros ici ce que je voulais faire c'etait de remplir les cases jouable de rectangle invisible et
+        normalement il y a un fonction dans pygame pour detecter les collisions de la souris avec un rectangle.
+        Je pense que ca servira pour jouer une piece justement.
+        |
+        v
+        '''
+        self.grid = [[pygame.Surface((55, 55), pygame.SRCALPHA)] * 15 for _ in range(15)]
+        for i in range(15):
+            for j in range(15):
+                pygame.draw.rect(self.grid[i][j], COLOR_TEST, (0,0,53.5,53.5))
+                self.screen.blit(self.grid[i][j], (50+i*53.5,50+j*53.5))
+
+                # self.circle_surface = pygame.Surface((S_HSIZE, S_VSIZE), pygame.SRCALPHA)
+                # pygame.draw.circle(self.circle_surface, COLOR_TEST, [75+53.5*i,75+53.5*j],23)
+                # self.screen.blit(self.circle_surface, (0, 0))
+
+        # flip
+        pygame.display.flip()
+
+
+    '''Ca c'est pour afficher les pions posés sur le plateau'''
+    def disp_put(self, color, x, y):
+        if color == 1:
+            piece = pygame.transform.scale(pygame.image.load("./game/assets/rblack.png"),(53.5,53.5))
+            self.screen.blit(piece, (50+x*53.5,50+y*53.5))
+        elif color == 2:
+            piece = pygame.transform.scale(pygame.image.load("./game/assets/rwhite.png"),(53.5,53.5))
+            self.screen.blit(piece, (50+x*53.5,50+y*53.5))
--- game/Renju.py
+++ game/Renju.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 import pygame
 from game import Board
 
@@ -10,6 +13,8 @@ class Renju:
     def __init__(self):
         self.board = Board()
         self.turn = 1
+        self.p1 = BLACK_PIECE
+        self.p2 = WHITE_PIECE
 
     def loc_check(self, X:int, Y:int) -> int :
         return self.board.matrix[X][Y]
@@ -25,25 +30,42 @@ class Renju:
         valid_move = False
         if self.turn % 2 == 1 :
             while not valid_move :
-                x_loc = int(input("X? "))
-                y_loc = int(input("Y? "))
+                # x_loc = int(input("X? "))
+                # y_loc = int(input("Y? "))
+
+                '''
+                |
+                v Juste pour tester l'affichage des pieces avec la fonction board.disp_put
+                '''
+                x_loc = random.randint(0,14)
+                y_loc = random.randint(0,14)
                 valid_move = self.place(BLACK_PIECE,x_loc,y_loc)
-                if self.five_check(BLACK_PIECE):
-                    return 1
+
+                if valid_move:
+                    self.board.disp_put(BLACK_PIECE,x_loc,y_loc)
+                    if self.five_check(BLACK_PIECE):
+                        return 1
+                else:
+                    print("Place deja occupée")
 
         else:
             while not valid_move :
-                x_loc = int(input("X? "))
-                y_loc = int(input("Y? "))
+                # x_loc = int(input("X? "))
+                # y_loc = int(input("Y? "))
+                '''idem ici'''
+                x_loc = random.randint(0,14)
+                y_loc = random.randint(0,14)
                 valid_move = self.place(WHITE_PIECE,x_loc,y_loc)
-                if not valid_move :
+
+                if valid_move:
+                    self.board.disp_put(WHITE_PIECE,x_loc,y_loc)
+                    if self.five_check(WHITE_PIECE):
+                        return 1
+                else:
                     print("Place deja occupée")
-                if self.five_check(WHITE_PIECE):
-                    return 1
 
         self.turn += 1
         return 0
-
 
     def five_check(self, p_color:int) -> bool:
         alignment = 0
@@ -93,7 +115,12 @@ class Renju:
         return False
 
     #TODO Implementer la sequence des 3 premiers tours.
-    #TODO Impelmenter les limitations des noirs. (fourchette 3 x 3 / 4 x 4, Overline)
+    def swap(self):
+        tmp = self.p2
+        self.p2 = self.p1
+        self.p1 = tmp
+        
+    #TODO Implementer les limitations des noirs. (fourchette 3 x 3 / 4 x 4, Overline)
 
     def run(self):
         running = True
@@ -104,15 +131,26 @@ class Renju:
             for e in self.board.matrix:
                 print(e)
 
+            '''
+            Y'a un probleme ici c'est que quand ca attend l'input de l'utilisateur trop longtemps alors l'interface
+            graphique va crash. Je suppose que c'est du au fait que ca bloque la boucle infini et que ca check pas les
+            events pendant trop de temps. Normalement ca sera corrigé j'imagine en implementant l'event ou il faudra
+            juste cliquer sur l'interface graphique qui ducoup n'interrompera normalement pas le programme en attendant
+            l'input utilisateur comme ici. 
+            |
+            v
+            '''
             p = self.turns()
+            time.sleep(0.3) # Juste histoire que ca spam pas les pions a la vitesse de la lumiere.
             if p != 0 :
                 print(f"Player {p} won !")
-                running = False
+                # running = False
+
 
         # TODO Interface Graphique
         # while running:
-        #     for ev in pygame.event.get():
-        #         if ev.type == pygame.QUIT:
-        #             running = False
-        #     self.board.draw()
-        # pygame.quit()
+            for ev in pygame.event.get():
+                if ev.type == pygame.QUIT:
+                    running = False
+            pygame.display.flip()
+        pygame.quit()
