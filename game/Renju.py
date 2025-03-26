4c6e41a3f8906c203cb535607c249661bf54a841
mariduartef <mariduarteferreira@gmail.com>
2025-03-26 15:15:41 +0100

    fork 3x3, overline et changement de la vérification des diagonales

--- game/Renju.py
+++ game/Renju.py
@@ -16,33 +16,34 @@ class Renju:
         self.p1 = BLACK_PIECE
         self.p2 = WHITE_PIECE
 
-    def loc_check(self, X:int, Y:int) -> int :
-        return self.board.matrix[X][Y]
+    def loc_check(self, x:int, y:int) -> int :
+        return self.board.matrix[x][y]
 
-    def place(self, p_color: int, X:int, Y:int) -> bool:
-        if(self.loc_check(X,Y) != 0):
+    def place(self, p_color: int, x:int, y:int) -> bool:
+        if self.loc_check(x,y) != 0:
             return False
 
-        self.board.matrix[X][Y] = p_color
+        self.board.matrix[x][y] = p_color
         return True
 
     def turns(self) -> int:
         valid_move = False
         if self.turn % 2 == 1 :
             while not valid_move :
-                # x_loc = int(input("X? "))
-                # y_loc = int(input("Y? "))
+                x_loc = int(input("X? "))
+                y_loc = int(input("Y? "))
 
                 '''
                 |
                 v Juste pour tester l'affichage des pieces avec la fonction board.disp_put
                 '''
-                x_loc = random.randint(0,14)
-                y_loc = random.randint(0,14)
+                #x_loc = random.randint(0,14)
+                #y_loc = random.randint(0,14)
                 valid_move = self.place(BLACK_PIECE,x_loc,y_loc)
 
                 if valid_move:
                     self.board.disp_put(BLACK_PIECE,x_loc,y_loc)
+                    print("fork 3 check " + str(self.fork_three_check(x_loc, y_loc)))
                     if self.five_check(BLACK_PIECE):
                         return 1
                 else:
@@ -50,11 +51,11 @@ class Renju:
 
         else:
             while not valid_move :
-                # x_loc = int(input("X? "))
-                # y_loc = int(input("Y? "))
+                x_loc = int(input("X? "))
+                y_loc = int(input("Y? "))
                 '''idem ici'''
-                x_loc = random.randint(0,14)
-                y_loc = random.randint(0,14)
+                #x_loc = random.randint(0,14)
+                #y_loc = random.randint(0,14)
                 valid_move = self.place(WHITE_PIECE,x_loc,y_loc)
 
                 if valid_move:
@@ -92,7 +93,7 @@ class Renju:
 
         #TODO + de tests sur les diagonales
 
-        # Diags /
+        '''# Diags /
         for col in range(4,15) :
             for row in range(0, col+1) :
                 if self.board.matrix[row][col-row] == p_color :
@@ -110,7 +111,28 @@ class Renju:
                     if alignment == 5 : return True
                 else :
                     alignment = 0
-            alignment = 0
+            alignment = 0'''
+
+        #Diags /
+        for col in range(14,3,-1) :
+            for row in range(14,3,-1) :
+                if ((self.board.matrix[row][col] == p_color) &
+                        (self.board.matrix[row-1][col-1] == p_color) &
+                        (self.board.matrix[row-2][col-2] == p_color) &
+                        (self.board.matrix[row-3][col-3] == p_color) &
+                        (self.board.matrix[row-4][col-4] == p_color)) :
+                    return True
+
+        #Diags \
+        for col in range(11):
+            for row in range(11):
+                if ((self.board.matrix[row][col] == p_color) &
+                        (self.board.matrix[row+1][col+1] == p_color) &
+                        (self.board.matrix[row+2][col+2] == p_color) &
+                        (self.board.matrix[row+3][col+3] == p_color) &
+                        (self.board.matrix[row+4][col+4] == p_color)) :
+                    return True
+
 
         return False
 
@@ -121,6 +143,185 @@ class Renju:
         self.p1 = tmp
         
     #TODO Implementer les limitations des noirs. (fourchette 3 x 3 / 4 x 4, Overline)
+    #TODO implementer pass (si les deux joueurs sautent le tour à la suite -> draw)
+
+    def overline_check(self, row:int) -> bool :
+        count_black = 0
+        first_is_black = (self.board.matrix[row][0]==BLACK_PIECE)
+        for col in range(7) : #on regarde les six premiers elements de la ligne
+            if self.board.matrix[row][col] == BLACK_PIECE :
+                count_black +=1
+        if count_black >= 5 :
+            return True
+        for col in range (7,15): #pour les elements suivants, on regarde seulement si l'element au debut des "six" n'est pas noir et si celui de la fin l'est aussi
+            if (not first_is_black) & self.board.matrix[row][col] == BLACK_PIECE :
+                count_black +=1
+                first_is_black = (self.board.matrix[row][col-6]==BLACK_PIECE)
+                if count_black >= 5 :
+                    return True
+        print("count black + " + str(count_black))
+        return False
+
+    def open_three_vertical_check(self, row: int, col: int) -> (bool,int,int,int,int):
+        assert row>=2 & row<=12
+
+        # On regarde si la ligne est proche du bord
+        if (row == 2) & (self.board.matrix[row - 1][col] == BLACK_PIECE):
+            return False, -1, -1, -1, -1
+        if (row == 12) & (self.board.matrix[row + 1][col] == BLACK_PIECE):
+            return False, -1, -1, -1, -1
+
+        #On cherche à voir s'il y a des pieces blanches au tour
+        if (self.board.matrix[row-1][col]==WHITE_PIECE) | (self.board.matrix[row+1][col]==WHITE_PIECE) :
+            return False, -1, -1, -1, -1
+        if (((self.board.matrix[row-2][col]==WHITE_PIECE) & (self.board.matrix[row-1][col]==BLACK_PIECE))
+                | (self.board.matrix[row+2][col]==WHITE_PIECE) & (self.board.matrix[row+1][col]==BLACK_PIECE)) :
+            return False, -1, -1, -1, -1
+
+        #On regarde la ligne
+        if self.board.matrix[row-1][col]==BLACK_PIECE :
+            if self.board.matrix[row-2][col]==BLACK_PIECE :
+                return True, row-1, col, row-2, col
+            if self.board.matrix[row+1][col]==BLACK_PIECE :
+                return True, row-1, col, row+1, col
+
+        if (self.board.matrix[row+1][col]==BLACK_PIECE) & (self.board.matrix[row+2][col]==BLACK_PIECE) :
+            return True, row+1, col, row+2, col
+
+        return False, -1, -1, -1, -1
+
+    def open_three_horizontal_check(self, row:int, col:int) -> (bool,int,int,int,int):
+        assert col >= 2 & col <= 12
+
+        # On regarde si la colonne est proche du bord
+        if (col == 2) & (self.board.matrix[row][col - 1] == BLACK_PIECE):
+            return False, -1, -1, -1, -1
+        if (col == 12) & (self.board.matrix[row][col + 1] == BLACK_PIECE):
+            return False, -1, -1, -1, -1
+
+        # On cherche à voir s'il y a des pieces blanches au tour
+        if (self.board.matrix[row][col-1] == WHITE_PIECE) | (self.board.matrix[row][col+1] == WHITE_PIECE):
+            return False, -1, -1, -1, -1
+        if (((self.board.matrix[row][col-2] == WHITE_PIECE) & (self.board.matrix[row][col-1] == BLACK_PIECE))
+                | (self.board.matrix[row][col + 2] == WHITE_PIECE) & (self.board.matrix[row][col + 1] == BLACK_PIECE)):
+            return False, -1, -1, -1, -1
+
+        #On regarde la colonne
+        if self.board.matrix[row][col-1] == BLACK_PIECE :
+            if self.board.matrix[row][col-2] == BLACK_PIECE :
+                return True, row, col-1, row, col-2
+            if self.board.matrix[row][col+1] == BLACK_PIECE :
+                return True, row, col-1, row, col+1
+
+        if (self.board.matrix[row][col+1] == BLACK_PIECE) & (self.board.matrix[row][col+2] == BLACK_PIECE):
+            return True, row, col+1, row, col+2
+
+        return False, -1, -1, -1, -1
+
+    #Diagonale 1 = \
+    def open_three_diagonal1_check(self, row:int, col:int) -> (bool, int, int, int, int):
+        assert col >= 2 & col <= 12 & row >= 2 & row <= 12
+
+        # On regarde si la diagonale est proche du bord
+        if (col == 2) & (self.board.matrix[row - 1][col - 1] == BLACK_PIECE):
+            return False, -1, -1, -1, -1
+        if (col == 12) & (self.board.matrix[row + 1][col + 1] == BLACK_PIECE):
+            return False, -1, -1, -1, -1
+
+        # On cherche à voir s'il y a des pieces blanches au tour
+        if (self.board.matrix[row - 1][col - 1] == WHITE_PIECE) | (self.board.matrix[row + 1][col + 1] == WHITE_PIECE):
+            return False, -1, -1, -1, -1
+        if (((self.board.matrix[row - 2][col - 2] == WHITE_PIECE) & (self.board.matrix[row - 1][col - 1] == BLACK_PIECE))
+                | (self.board.matrix[row + 2][col + 2] == WHITE_PIECE) & (self.board.matrix[row + 1][col + 1] == BLACK_PIECE)):
+            return False, -1, -1, -1, -1
+
+
+        #On regarde la diagonale
+        if self.board.matrix[row - 1][col - 1] == BLACK_PIECE :
+            if self.board.matrix[row - 2][col - 2] == BLACK_PIECE :
+                return True, row-1, col-1, row-2, col-2
+            if self.board.matrix[row + 1][col + 1] == BLACK_PIECE :
+                return True, row-1, col-1, row+1, col+1
+
+        if (self.board.matrix[row + 1][col + 1] == BLACK_PIECE) & (self.board.matrix[row + 2][col + 2] == BLACK_PIECE):
+            return True, row+1, col+1, row+2, col+2
+
+        return False, -1, -1, -1, -1
+
+    #Diagonale 2 = /
+    def open_three_diagonal2_check(self, row:int, col:int) -> (bool, int, int, int, int):
+        assert col >= 2 & col <= 12 & row >= 2 & row <= 12
+
+        # On regarde si la diagonale est proche du bord
+        if (col == 2) & (self.board.matrix[row + 1][col - 1] == BLACK_PIECE):
+            return False, -1, -1, -1, -1
+        if (col == 12) & (self.board.matrix[row - 1][col + 1] == BLACK_PIECE):
+            return False, -1, -1, -1, -1
+
+        # On cherche à voir s'il y a des pieces blanches au tour
+        if (self.board.matrix[row + 1][col - 1] == WHITE_PIECE) | (self.board.matrix[row - 1][col + 1] == WHITE_PIECE):
+            return False, -1, -1, -1, -1
+        if (((self.board.matrix[row + 2][col - 2] == WHITE_PIECE) & (self.board.matrix[row + 1][col - 1] == BLACK_PIECE))
+                | (self.board.matrix[row - 2][col + 2] == WHITE_PIECE) & (self.board.matrix[row - 1][col + 1] == BLACK_PIECE)):
+            return False, -1, -1, -1, -1
+
+
+        #On regarde la diagonale
+        if self.board.matrix[row - 1][col + 1] == BLACK_PIECE :
+            if self.board.matrix[row - 2][col + 2] == BLACK_PIECE :
+                return True, row-1, col+1, row-2, col+2
+            if self.board.matrix[row + 1][col - 1] == BLACK_PIECE :
+                return True, row-1,col+1, row+1, col-1
+
+        if (self.board.matrix[row + 1][col - 1] == BLACK_PIECE) & (self.board.matrix[row + 2][col - 2] == BLACK_PIECE):
+            return True, row+1, col-1, row+2, col-2
+
+        return False, -1, -1, -1, -1
+
+    #not_to_test doit valoir 1(vertical), 2(horizontal), 3(diag1) ou 4(diag4)
+    def has_open_three(self, row:int, col:int, not_to_test:int)->bool:
+        assert not_to_test==1 | not_to_test==2 | not_to_test==3 | not_to_test==4
+        if (row < 2) | (row > 12) | (col < 2) | (col > 12):
+            return False
+        if (self.open_three_vertical_check(row, col)) & (not_to_test!=1):
+            return True
+        if (self.open_three_horizontal_check(row, col)) & (not_to_test != 2):
+            return True
+        if (self.open_three_diagonal1_check(row, col)) & (not_to_test!=3):
+            return True
+        if (self.open_three_diagonal2_check(row, col)) & (not_to_test!=4):
+            return True
+        return False
+
+    def fork_three_check(self, row:int, col:int) -> bool:
+        if (row < 2) | (row > 12) | (col < 2) | (col > 12):  # nous n'avons pas un open 3 lorsqu'on est on proche des bords
+            return False
+
+        open_three = self.open_three_vertical_check(row,col)
+        if  open_three[0]:
+            if ((self.has_open_three(open_three[1],open_three[2],1)) |
+                    (self.has_open_three(open_three[3],open_three[4],1))):
+                    return True
+
+        open_three = self.open_three_horizontal_check(row, col)
+        if open_three[0]:
+            if ((self.has_open_three(open_three[1], open_three[2],2)) |
+                    (self.has_open_three(open_three[3], open_three[4],2))):
+                return True
+
+        open_three = self.open_three_diagonal1_check(row, col)
+        if open_three[0]:
+            if ((self.has_open_three(open_three[1], open_three[2],3)) |
+                    (self.has_open_three(open_three[3], open_three[4],3))):
+                return True
+
+        open_three = self.open_three_diagonal2_check(row, col)
+        if open_three[0]:
+            if ((self.has_open_three(open_three[1], open_three[2],4)) |
+                    (self.has_open_three(open_three[3], open_three[4],4))):
+                return True
+
+        return False
 
     def run(self):
         running = True
