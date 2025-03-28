af3158ea5bbb9d17d02a88c7201722df998dc987
mariduartef <mariduarteferreira@gmail.com>
2025-03-28 21:57:43 +0100

    chagement des & et | par and et or

--- game/Renju.py
+++ game/Renju.py
@@ -30,7 +30,7 @@ class Renju:
                 raise InvalidMove("Case impossible.")
 
             self.board.scene.update_case(BLACK_PIECE, x_loc, y_loc)
-            # print("fork 3 check " + str(self.fork_three_check(x_loc, y_loc))) // Retourne une erreur
+            print("fork 3 check " + str(self.fork_three_check(x_loc, y_loc)))
             if self.five_check(BLACK_PIECE):
                 return 1
 
@@ -94,20 +94,20 @@ class Renju:
         #Diags /
         for col in range(14,-1,-1) :
             for row in range(14,-1,-1) :
-                if ((self.board.matrix[row][col] == p_color) &
-                        (self.board.matrix[row-1][col-1] == p_color) &
-                        (self.board.matrix[row-2][col-2] == p_color) &
-                        (self.board.matrix[row-3][col-3] == p_color) &
+                if ((self.board.matrix[row][col] == p_color) and
+                        (self.board.matrix[row-1][col-1] == p_color) and
+                        (self.board.matrix[row-2][col-2] == p_color) and
+                        (self.board.matrix[row-3][col-3] == p_color) and
                         (self.board.matrix[row-4][col-4] == p_color)) :
                     return True
 
         #Diags \
         for col in range(11):
             for row in range(11):
-                if ((self.board.matrix[row][col] == p_color) &
-                        (self.board.matrix[row+1][col+1] == p_color) &
-                        (self.board.matrix[row+2][col+2] == p_color) &
-                        (self.board.matrix[row+3][col+3] == p_color) &
+                if ((self.board.matrix[row][col] == p_color) and
+                        (self.board.matrix[row+1][col+1] == p_color) and
+                        (self.board.matrix[row+2][col+2] == p_color) and
+                        (self.board.matrix[row+3][col+3] == p_color) and
                         (self.board.matrix[row+4][col+4] == p_color)) :
                     return True
 
@@ -132,7 +132,7 @@ class Renju:
         if count_black >= 5 :
             return True
         for col in range (7,15): #pour les elements suivants, on regarde seulement si l'element au debut des "six" n'est pas noir et si celui de la fin l'est aussi
-            if (not first_is_black) & self.board.matrix[row][col] == BLACK_PIECE :
+            if (not first_is_black) and self.board.matrix[row][col] == BLACK_PIECE :
                 count_black +=1
                 first_is_black = (self.board.matrix[row][col-6]==BLACK_PIECE)
                 if count_black >= 5 :
@@ -141,19 +141,19 @@ class Renju:
         return False
 
     def open_three_vertical_check(self, row: int, col: int) -> (bool,int,int,int,int):
-        assert row>=2 & row<=12
+        assert row>=2 and row<=12
 
         # On regarde si la ligne est proche du bord
-        if (row == 2) & (self.board.matrix[row - 1][col] == BLACK_PIECE):
+        if (row == 2) and (self.board.matrix[row - 1][col] == BLACK_PIECE):
             return False, -1, -1, -1, -1
-        if (row == 12) & (self.board.matrix[row + 1][col] == BLACK_PIECE):
+        if (row == 12) and (self.board.matrix[row + 1][col] == BLACK_PIECE):
             return False, -1, -1, -1, -1
 
         #On cherche à voir s'il y a des pieces blanches au tour
-        if (self.board.matrix[row-1][col]==WHITE_PIECE) | (self.board.matrix[row+1][col]==WHITE_PIECE) :
+        if (self.board.matrix[row-1][col]==WHITE_PIECE) or (self.board.matrix[row+1][col]==WHITE_PIECE) :
             return False, -1, -1, -1, -1
-        if (((self.board.matrix[row-2][col]==WHITE_PIECE) & (self.board.matrix[row-1][col]==BLACK_PIECE))
-                | (self.board.matrix[row+2][col]==WHITE_PIECE) & (self.board.matrix[row+1][col]==BLACK_PIECE)) :
+        if (((self.board.matrix[row-2][col]==WHITE_PIECE) and (self.board.matrix[row-1][col]==BLACK_PIECE))
+                or (self.board.matrix[row+2][col]==WHITE_PIECE) and (self.board.matrix[row+1][col]==BLACK_PIECE)) :
             return False, -1, -1, -1, -1
 
         #On regarde la ligne
@@ -163,25 +163,25 @@ class Renju:
             if self.board.matrix[row+1][col]==BLACK_PIECE :
                 return True, row-1, col, row+1, col
 
-        if (self.board.matrix[row+1][col]==BLACK_PIECE) & (self.board.matrix[row+2][col]==BLACK_PIECE) :
+        if (self.board.matrix[row+1][col]==BLACK_PIECE) and (self.board.matrix[row+2][col]==BLACK_PIECE) :
             return True, row+1, col, row+2, col
 
         return False, -1, -1, -1, -1
 
     def open_three_horizontal_check(self, row:int, col:int) -> (bool,int,int,int,int):
-        assert col >= 2 & col <= 12
+        assert col >= 2 and col <= 12
 
         # On regarde si la colonne est proche du bord
-        if (col == 2) & (self.board.matrix[row][col - 1] == BLACK_PIECE):
+        if (col == 2) and (self.board.matrix[row][col - 1] == BLACK_PIECE):
             return False, -1, -1, -1, -1
-        if (col == 12) & (self.board.matrix[row][col + 1] == BLACK_PIECE):
+        if (col == 12) and (self.board.matrix[row][col + 1] == BLACK_PIECE):
             return False, -1, -1, -1, -1
 
         # On cherche à voir s'il y a des pieces blanches au tour
-        if (self.board.matrix[row][col-1] == WHITE_PIECE) | (self.board.matrix[row][col+1] == WHITE_PIECE):
+        if (self.board.matrix[row][col-1] == WHITE_PIECE) or (self.board.matrix[row][col+1] == WHITE_PIECE):
             return False, -1, -1, -1, -1
-        if (((self.board.matrix[row][col-2] == WHITE_PIECE) & (self.board.matrix[row][col-1] == BLACK_PIECE))
-                | (self.board.matrix[row][col + 2] == WHITE_PIECE) & (self.board.matrix[row][col + 1] == BLACK_PIECE)):
+        if (((self.board.matrix[row][col-2] == WHITE_PIECE) and (self.board.matrix[row][col-1] == BLACK_PIECE))
+                or (self.board.matrix[row][col + 2] == WHITE_PIECE) and (self.board.matrix[row][col + 1] == BLACK_PIECE)):
             return False, -1, -1, -1, -1
 
         #On regarde la colonne
@@ -191,26 +191,26 @@ class Renju:
             if self.board.matrix[row][col+1] == BLACK_PIECE :
                 return True, row, col-1, row, col+1
 
-        if (self.board.matrix[row][col+1] == BLACK_PIECE) & (self.board.matrix[row][col+2] == BLACK_PIECE):
+        if (self.board.matrix[row][col+1] == BLACK_PIECE) and (self.board.matrix[row][col+2] == BLACK_PIECE):
             return True, row, col+1, row, col+2
 
         return False, -1, -1, -1, -1
 
     #Diagonale 1 = \
     def open_three_diagonal1_check(self, row:int, col:int) -> (bool, int, int, int, int):
-        assert col >= 2 & col <= 12 & row >= 2 & row <= 12
+        assert col >= 2 and col <= 12 and row >= 2 and row <= 12
 
         # On regarde si la diagonale est proche du bord
-        if (col == 2) & (self.board.matrix[row - 1][col - 1] == BLACK_PIECE):
+        if (col == 2) and (self.board.matrix[row - 1][col - 1] == BLACK_PIECE):
             return False, -1, -1, -1, -1
-        if (col == 12) & (self.board.matrix[row + 1][col + 1] == BLACK_PIECE):
+        if (col == 12) and (self.board.matrix[row + 1][col + 1] == BLACK_PIECE):
             return False, -1, -1, -1, -1
 
         # On cherche à voir s'il y a des pieces blanches au tour
-        if (self.board.matrix[row - 1][col - 1] == WHITE_PIECE) | (self.board.matrix[row + 1][col + 1] == WHITE_PIECE):
+        if (self.board.matrix[row - 1][col - 1] == WHITE_PIECE) or (self.board.matrix[row + 1][col + 1] == WHITE_PIECE):
             return False, -1, -1, -1, -1
-        if (((self.board.matrix[row - 2][col - 2] == WHITE_PIECE) & (self.board.matrix[row - 1][col - 1] == BLACK_PIECE))
-                | (self.board.matrix[row + 2][col + 2] == WHITE_PIECE) & (self.board.matrix[row + 1][col + 1] == BLACK_PIECE)):
+        if (((self.board.matrix[row - 2][col - 2] == WHITE_PIECE) and (self.board.matrix[row - 1][col - 1] == BLACK_PIECE))
+                or (self.board.matrix[row + 2][col + 2] == WHITE_PIECE) and (self.board.matrix[row + 1][col + 1] == BLACK_PIECE)):
             return False, -1, -1, -1, -1
 
 
@@ -221,26 +221,26 @@ class Renju:
             if self.board.matrix[row + 1][col + 1] == BLACK_PIECE :
                 return True, row-1, col-1, row+1, col+1
 
-        if (self.board.matrix[row + 1][col + 1] == BLACK_PIECE) & (self.board.matrix[row + 2][col + 2] == BLACK_PIECE):
+        if (self.board.matrix[row + 1][col + 1] == BLACK_PIECE) and (self.board.matrix[row + 2][col + 2] == BLACK_PIECE):
             return True, row+1, col+1, row+2, col+2
 
         return False, -1, -1, -1, -1
 
     #Diagonale 2 = /
     def open_three_diagonal2_check(self, row:int, col:int) -> (bool, int, int, int, int):
-        assert col >= 2 & col <= 12 & row >= 2 & row <= 12
+        assert col >= 2 and col <= 12 and row >= 2 and row <= 12
 
         # On regarde si la diagonale est proche du bord
-        if (col == 2) & (self.board.matrix[row + 1][col - 1] == BLACK_PIECE):
+        if (col == 2) and (self.board.matrix[row + 1][col - 1] == BLACK_PIECE):
             return False, -1, -1, -1, -1
-        if (col == 12) & (self.board.matrix[row - 1][col + 1] == BLACK_PIECE):
+        if (col == 12) and (self.board.matrix[row - 1][col + 1] == BLACK_PIECE):
             return False, -1, -1, -1, -1
 
         # On cherche à voir s'il y a des pieces blanches au tour
-        if (self.board.matrix[row + 1][col - 1] == WHITE_PIECE) | (self.board.matrix[row - 1][col + 1] == WHITE_PIECE):
+        if (self.board.matrix[row + 1][col - 1] == WHITE_PIECE) or (self.board.matrix[row - 1][col + 1] == WHITE_PIECE):
             return False, -1, -1, -1, -1
-        if (((self.board.matrix[row + 2][col - 2] == WHITE_PIECE) & (self.board.matrix[row + 1][col - 1] == BLACK_PIECE))
-                | (self.board.matrix[row - 2][col + 2] == WHITE_PIECE) & (self.board.matrix[row - 1][col + 1] == BLACK_PIECE)):
+        if (((self.board.matrix[row + 2][col - 2] == WHITE_PIECE) and (self.board.matrix[row + 1][col - 1] == BLACK_PIECE))
+                or (self.board.matrix[row - 2][col + 2] == WHITE_PIECE) and (self.board.matrix[row - 1][col + 1] == BLACK_PIECE)):
             return False, -1, -1, -1, -1
 
 
@@ -251,51 +251,51 @@ class Renju:
             if self.board.matrix[row + 1][col - 1] == BLACK_PIECE :
                 return True, row-1,col+1, row+1, col-1
 
-        if (self.board.matrix[row + 1][col - 1] == BLACK_PIECE) & (self.board.matrix[row + 2][col - 2] == BLACK_PIECE):
+        if (self.board.matrix[row + 1][col - 1] == BLACK_PIECE) and (self.board.matrix[row + 2][col - 2] == BLACK_PIECE):
             return True, row+1, col-1, row+2, col-2
 
         return False, -1, -1, -1, -1
 
-    #not_to_test doit valoir 1(vertical), 2(horizontal), 3(diag1) ou 4(diag4)
+    #not_to_test doit valoir 1(vertical), 2(horizontal), 3(diag1) ou 4(diag2)
     def has_open_three(self, row:int, col:int, not_to_test:int)->bool:
-        assert not_to_test==1 | not_to_test==2 | not_to_test==3 | not_to_test==4
-        if (row < 2) | (row > 12) | (col < 2) | (col > 12):
+        assert not_to_test==1 or not_to_test==2 or not_to_test==3 or not_to_test==4
+        if (row < 2) or (row > 12) or (col < 2) or (col > 12):
             return False
-        if (self.open_three_vertical_check(row, col)) & (not_to_test!=1):
+        if (self.open_three_vertical_check(row, col)) and (not_to_test!=1):
             return True
-        if (self.open_three_horizontal_check(row, col)) & (not_to_test != 2):
+        if (self.open_three_horizontal_check(row, col)) and (not_to_test != 2):
             return True
-        if (self.open_three_diagonal1_check(row, col)) & (not_to_test!=3):
+        if (self.open_three_diagonal1_check(row, col)) and (not_to_test!=3):
             return True
-        if (self.open_three_diagonal2_check(row, col)) & (not_to_test!=4):
+        if (self.open_three_diagonal2_check(row, col)) and (not_to_test!=4):
             return True
         return False
 
     def fork_three_check(self, row:int, col:int) -> bool:
-        if (row < 2) | (row > 12) | (col < 2) | (col > 12):  # nous n'avons pas un open 3 lorsqu'on est on proche des bords
+        if (row < 2) or (row > 12) or (col < 2) or (col > 12):  # nous n'avons pas un open 3 lorsqu'on est on proche des bords
             return False
 
         open_three = self.open_three_vertical_check(row,col)
         if  open_three[0]:
-            if ((self.has_open_three(open_three[1],open_three[2],1)) |
+            if ((self.has_open_three(open_three[1],open_three[2],1)) or
                     (self.has_open_three(open_three[3],open_three[4],1))):
                     return True
 
         open_three = self.open_three_horizontal_check(row, col)
         if open_three[0]:
-            if ((self.has_open_three(open_three[1], open_three[2],2)) |
+            if ((self.has_open_three(open_three[1], open_three[2],2)) or
                     (self.has_open_three(open_three[3], open_three[4],2))):
                 return True
 
         open_three = self.open_three_diagonal1_check(row, col)
         if open_three[0]:
-            if ((self.has_open_three(open_three[1], open_three[2],3)) |
+            if ((self.has_open_three(open_three[1], open_three[2],3)) or
                     (self.has_open_three(open_three[3], open_three[4],3))):
                 return True
 
         open_three = self.open_three_diagonal2_check(row, col)
         if open_three[0]:
-            if ((self.has_open_three(open_three[1], open_three[2],4)) |
+            if ((self.has_open_three(open_three[1], open_three[2],4)) or
                     (self.has_open_three(open_three[3], open_three[4],4))):
                 return True
 
